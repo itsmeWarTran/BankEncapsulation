192fea85526506c04cba492df0e92240eec8a517
Warren Tran <warrentran2015@hotmail.com>
2022-06-13 17:31:39 -0400

    descriptive message

---
+++ BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+
+        
+        private double balance;
+
+        public BankAccount(double amount)
+        {
+            balance = amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("How much do you have in your bank account?");
+            double howMuch = Double.Parse(Console.ReadLine());
+
+          
+
+            var money = new BankAccount(howMuch);
+
+            Console.WriteLine("Do you want to deposit?");
+            string wantsDeposit = Console.ReadLine();
+
+            if (wantsDeposit == "yes")
+            {
+                Console.WriteLine("How much do you want to deposit?");
+                double muchDeposit = Double.Parse(Console.ReadLine());
+                money.Deposit(muchDeposit);
+                var getAmount = money.GetBalance();
+                Console.WriteLine("You total amount is $" + getAmount);
+            }
+            else
+            {
+                Console.WriteLine("OK! Have a great day");
+            }
+           
+
+            
+
+        }
+    }
+}
